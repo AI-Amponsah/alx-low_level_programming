c7db5f73b4502d946009c62a80c053c0fb50d9f0
Isaac Amponsah <Yawamp27@gmail.com>
2022-10-13 21:50:14 +0000

    Push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+include <stdio.h>
+#include <unistd.h>
 
 /**
-  *main - Entry
-  *
-  *Return: Always 0
-  */
+ * main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
