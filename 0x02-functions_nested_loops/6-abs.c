e458b9480c74ba9a986443bc64c94195d2205870
Isaac Amponsah <Yawamp27@gmail.com>
2022-10-18 20:02:47 +0000

    Push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _abs - Prints absolute of a number
+  * Return: Always 0
+  */
+
+int _abs(int i)
+{
+
+	if ((i < 0) || (i == 0) || (i > 0))
+		_putchar(i);
+	return (0);
+}
