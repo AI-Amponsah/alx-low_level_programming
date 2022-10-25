ec9cea6d38eb40ca2011e02e35b97aad0feea46c
Isaac Amponsah <Yawamp27@gmail.com>
2022-10-25 02:10:09 +0000

    Pointer

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 
 	unsigned int i;
 
-	for (i = 0; i <= strlen(str + 1); i++)
+	for (i = 0; i <= strlen(str); i++)
 	{
 		if ((i != '\0') && (i >= 5) && (i <= 9))
 			_putchar(str[i]);
