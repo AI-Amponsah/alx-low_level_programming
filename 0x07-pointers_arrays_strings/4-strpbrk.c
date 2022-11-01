79e142e71bc3efc7430dc88718dc562d5daf4b6e
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-01 03:04:37 +0000

    String

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strpbrk - fills a block of memory with a character of n bytes
+ * @s: String in consideration
+ * @accept: matching string
+ * Return: A pointer to s
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+
+for (i = 0; i <= strlen(s); i++)
+{
+for (j = 0; accept[j]; j++)
+{
+if (accept[j] == s[i])
+{
+return (s + 2);
+}
+}
+}
+return (0);
+}
