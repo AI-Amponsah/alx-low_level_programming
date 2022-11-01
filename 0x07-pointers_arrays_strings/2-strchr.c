9b9b7854075e31480c52348c9ebd0732131e6ea1
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-01 00:54:31 +0000

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,12 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
-for (i = 0; i <= strlen(s); i++)
+for (i = 0; i < strlen(s + 1); i++)
 {
 if (s[i] == c)
 {
 return (s + 2);
 }
-
 }
 return (!c ? s : NULL);
 }
