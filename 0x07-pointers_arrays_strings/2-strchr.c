7a8e9c397b6e4afe26c04a5d367a9ee778886fcf
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-01 00:41:21 +0000

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strchr - points to afirst occurrence of a  char in a string
+* @s: string
+* @c: character
+* Return: string pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+for (i = 0; i <= strlen(s); i++)
+{
+if (s[i] == c)
+{
+return (s + 2);
+}
+
+}
+return (NULL);
+}
