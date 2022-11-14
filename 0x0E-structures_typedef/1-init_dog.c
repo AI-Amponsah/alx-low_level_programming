6de3447e16b3e706ef5bdff62a651a9a9dba92e2
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-14 17:45:23 +0000

    Initialize

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,12 +6,18 @@
  * @name: Item name
  * @age: age
  * @owner: owner
- *@d: pointer to struct
+ * @d: pointer to struct
+ *Return: void
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
