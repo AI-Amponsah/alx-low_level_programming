ba8553b7f421f9cafbdd080b28b94603dd5bb8ca
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-24 12:45:49 +0000

    Linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* print_list - prints elements in struct
+*@h: struct pointer
+*Return: size_t
+*/
+
+size_t print_list(const list_t *h)
+{
+int i = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+h = h->next;
+}
+else
+{
+printf(" [%u] %s\n", h->len, h->str);
+h = h->next;
+}
+i++;
+}
+return (i);
+}
