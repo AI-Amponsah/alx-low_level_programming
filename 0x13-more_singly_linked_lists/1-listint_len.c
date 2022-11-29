11d0fc46d5f0720a14d0ffa92100a5be28c4ba7d
Isaac Amponsah <Yawamp27@gmail.com>
2022-11-29 00:22:20 +0000

    num

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  *listint_len - prints list in a linked list
+  *@h: head pointer
+  *
+  *Return: size_t
+  */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
