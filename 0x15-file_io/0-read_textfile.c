ffdd59cc6fc816431f856d38eac0d7f7311ffaf6
Isaac Amponsah <Yawamp27@gmail.com>
2022-12-06 14:33:13 +0000

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * read_textfile - reads and writes a text file Stdout 
+ * read_textfile - reads and writes a text file Stdout
  *@filename: name of file
  *@letters: length of letters to be read
  * Return: Number of letters read
@@ -8,7 +8,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t r , w;
+	ssize_t r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -22,13 +22,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	buffer = malloc(sizeof(char) * (letters));
-	if(buffer == NULL)
+
+	if (buffer == NULL)
 	{
 		return (0);
 	}
 	r = read(fd, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
-	close (fd);
+	close(fd);
 	free(buffer);
 
 	return (w);
